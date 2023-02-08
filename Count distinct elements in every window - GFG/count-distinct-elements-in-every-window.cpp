5b533e4b2c70c8c3502dad40cf0f71082c06a30d
Anant Preet Singh Reen <72596478+anantpreet26@users.noreply.github.com>
2023-02-08 11:19:44 +0530

    Added solution - LeetHub

---
+++ Count distinct elements in every window - GFG/count-distinct-elements-in-every-window.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+class Solution{
+  public:
+    vector <int> countDistinct (int A[], int n, int k)
+    {
+        vector<int>res;
+        unordered_map<int,int>map;
+        for(auto i=0;i<k;i++){
+            map[A[i]]++;
+        }
+        res.push_back(map.size());
+        
+        for(int j=k,i=0;j<n;j++,i++){
+            map[A[i]]--;
+            if(map[A[i]]==0)
+            map.erase(A[i]);
+            map[A[j]]++;
+            res.push_back(map.size());
+            
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+        	cin >> a[i];
+        Solution obj;
+        vector <int> result = obj.countDistinct(a, n, k);
+        for (int i : result) 
+        	cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
